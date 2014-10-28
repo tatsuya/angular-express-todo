814d6d4511ebfaec6b889b55ce18642838d96723
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2014-10-28 14:20:33 +0900

    redirect all paths to the index

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use('/', routes);
 app.use('/api', api);
 app.use('/partials', partials);
 
+// redirect all others to the index
+app.use('*', routes);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
