5319ad134f61a085ac26e0bf45c3cdec201aaec6
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-04-02 18:03:18 +0900

    make angular's DI minification safe

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,57 +4,68 @@
 
 /* Controllers */
 
+
+// Minification Safe Dependency Injection in AngularJS - http://tutorials.jenkov.com/angularjs/dependency-injection.html#minification-safe-dependency-injection
+
 angular.module('todoApp.controllers', []).
-  controller('IndexCtrl', function($scope, $http, $location) {
-    $http.get('/api/todos').
-      success(function(data) {
-        $scope.todos = data.todos;
-      });
-    $scope.toggleTodo = function(todo) {
-      todo.completed = !todo.completed;
-      $http.put('/api/todo/' + todo.id, todo).
-        success(function() {
-          $location.url('/');
+  controller('IndexCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
+      $http.get('/api/todos').
+        success(function(data) {
+          $scope.todos = data.todos;
         });
-    };
-  }).
-  controller('AddTodoCtrl', function($scope, $http, $location) {
-    $scope.form = {};
-    $scope.submitTodo = function() {
-      $http.post('/api/todo', $scope.form).
-        success(function() {
-          $location.path('/');
+      $scope.toggleTodo = function(todo) {
+        todo.completed = !todo.completed;
+        $http.put('/api/todo/' + todo.id, todo).
+          success(function() {
+            $location.url('/');
+          });
+      };
+    }
+  ]).
+  controller('AddTodoCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
+      $scope.form = {};
+      $scope.submitTodo = function() {
+        $http.post('/api/todo', $scope.form).
+          success(function() {
+            $location.path('/');
+          });
+      };
+    }
+  ]).
+  controller('EditTodoCtrl', ['$scope', '$http', '$location', '$routeParams',
+    function($scope, $http, $location, $routeParams) {
+      $scope.form = {};
+      $http.get('/api/todo/' + $routeParams.id).
+        success(function(data) {
+          $scope.form = data.todo;
         });
-    };
-  }).
-  controller('EditTodoCtrl', function($scope, $http, $location, $routeParams) {
-    $scope.form = {};
-    $http.get('/api/todo/' + $routeParams.id).
-      success(function(data) {
-        $scope.form = data.todo;
-      });
 
-    $scope.editTodo = function() {
-      $http.put('/api/todo/' + $routeParams.id, $scope.form).
-        success(function() {
-          $location.url('/');
+      $scope.editTodo = function() {
+        $http.put('/api/todo/' + $routeParams.id, $scope.form).
+          success(function() {
+            $location.url('/');
+          });
+      };
+    }
+  ]).
+  controller('DeleteTodoCtrl', ['$scope', '$http', '$location', '$routeParams',
+    function($scope, $http, $location, $routeParams) {
+      $http.get('/api/todo/' + $routeParams.id).
+        success(function(data) {
+          $scope.todo = data.todo;
         });
-    };
-  }).
-  controller('DeleteTodoCtrl', function($scope, $http, $location, $routeParams) {
-    $http.get('/api/todo/' + $routeParams.id).
-      success(function(data) {
-        $scope.todo = data.todo;
-      });
 
-    $scope.deleteTodo = function() {
-      $http.delete('/api/todo/' + $routeParams.id).
-        success(function() {
-          $location.url('/');
-        });
-    };
+      $scope.deleteTodo = function() {
+        $http.delete('/api/todo/' + $routeParams.id).
+          success(function() {
+            $location.url('/');
+          });
+      };
 
-    $scope.home =  function() {
-      $location.url('/');
-    };
-  });
+      $scope.home =  function() {
+        $location.url('/');
+      };
+    }
+  ]);
